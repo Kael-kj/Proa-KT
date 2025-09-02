dbb2e3af681169d6d2bd118c794aa7575eefd4e6
Mateus Kenji <126969517+Kael-kj@users.noreply.github.com>
2025-09-02 12:48:04 -0300

    Add files via upload

--- Hotel v2/src/Main.kt
+++ Hotel v2/src/Main.kt
@@ -16,6 +16,22 @@ data class Hospedagem(
     val quarto: Int,
     val listaHospedes: MutableList<String>
 )
+data class Evento(
+    val id: Int,
+    val empresa: String,
+    val convidados: Int,
+    val local: String,
+    val dia: String,
+    val hora: Int,
+    val duracao: Int,
+    val totalGarcons: Int,
+    val custoGarcons: Double,
+    val custoBuffet: Double,
+    val custoTotal: Double,
+    val cadeirasExtras: Int
+)
+val eventos = mutableListOf<Evento>()
+var contadorEventos = 1
 
 object UserSession {
     var currentUser: String? = null
@@ -76,6 +92,7 @@ fun homepage(scanner: Scanner) {
     println("2- Gerenciar hóspedes")
     println("3- Logout")
     println("4- Sair do programa")
+    println("5- Pesquisar hospedagens")
     val escolha = scanner.nextLine()
 
     when (escolha) {
@@ -94,6 +111,14 @@ fun homepage(scanner: Scanner) {
         "4" -> {
             println("Saindo do programa...")
             exitProcess(0)
+        }
+        "5" -> {
+            pesquisarHospedagem(scanner)
+            homepage(scanner)
+        }
+        "6"->{
+            cadastrarEvento(scanner)
+            homepage(scanner)
         }
         else -> {
             println("Opção inválida!")
@@ -243,6 +268,168 @@ fun cadastrarHospedes(scanner: Scanner) {
         }
     }
 }
+
+fun pesquisarHospedagem(scanner: Scanner){
+    println("\n=== PESQUISAR HOSPEDAGEM ===")
+    println("1- Por nome do hóspede principal")
+    println("2- Por número do quarto")
+    println("3- Por ID do cadastro")
+    println("4- Voltar")
+    print("Escolha: ")
+    when(scanner.nextLine()) {
+        "1" -> {
+            print("Digite o nome: ")
+            val nomeBusca = scanner.nextLine()
+            val resultados = hospedagens.filter { it.nome.contains(nomeBusca, ignoreCase = true) }
+            if (resultados.isEmpty()) {
+                println("Nenhum hóspede encontrado.")
+            } else resultados.forEach { mostrarResumoHospedagem(it) }
+        }
+
+        "2" -> {
+            print("Digite o número do quarto: ")
+            val quartoBusca = scanner.nextLine().toIntOrNull()
+            if (quartoBusca == null) {
+                println("Entrada inválida!")
+            } else {
+                val resultados = hospedagens.filter { it.quarto == quartoBusca }
+                if (resultados.isEmpty()) println("Nenhum hóspede encontrado nesse quarto.")
+                else resultados.forEach { mostrarResumoHospedagem(it) }
+            }
+        }
+
+        "3" -> {
+            print("Digite o ID do cadastro: ")
+            val idBusca = scanner.nextLine().toIntOrNull()
+            if (idBusca == null) {
+                println("Entrada inválida!")
+            } else {
+                val resultado = hospedagens.find { it.id == idBusca }
+                if (resultado == null) println("Nenhum cadastro encontrado com esse ID.")
+                else mostrarResumoHospedagem(resultado)
+            }
+        }
+        "4"-> return
+        else-> println("Opção inválida!")
+    }
+}
+
+fun mostrarResumoHospedagem(h: Hospedagem) {
+    println("""
+        --- HOSPEDAGEM ENCONTRADA ---
+        ID: ${h.id}
+        Hóspede principal: ${h.nome}
+        Dias: ${h.dias}
+        Quarto: ${h.quarto}
+        Total: R$ ${h.total}
+        Hóspedes: ${h.listaHospedes.joinToString(", ")}
+        -----------------------------
+    """.trimIndent())
+}
+
+fun cadastrarEvento(scanner:Scanner){
+    println("=== CADASTRO DE EVENTO ===")
+    print("Qual o número de convidados para o evento? ")
+    val convidados = scanner.nextLine().toInt()
+
+    if (convidados <= 0 || convidados > 350) {
+        println("Número de convidados inválido!")
+        return
+    }
+    var local : String
+    var cadeirasExtras = 0
+
+    if(convidados<= 220){
+        local = "Adutório Laranja"
+        if(convidados > 150) cadeirasExtras = convidados - 150
+        println("use o $local (inclua $cadeirasExtras cadeiras extras)")
+    }else {
+        local = "Auditório Colorado"
+        println("Use o $local")
+    }
+
+    print("Qual o dia do evento? (dias da semana)")
+    val dia = scanner.nextLine().lowercase()
+    println("Qual hora do evento? (apenas hora cheia)")
+    val hora = scanner.nextLine().toInt()
+    val disponivel = when(dia){
+        "segunda", "terca", "quarta", "quinta", "sexta" -> hora in 7..23
+        "sabado", "domingo" -> hora in 7..15
+        else -> false
+    }
+
+    if (!disponivel) {
+        println("Auditório indisponível")
+        return
+    }
+
+    print("Qual o nome da empresa? ")
+    val empresa = scanner.nextLine()
+
+    println("Auditório reservado para $empresa. $dia às ${hora}h.")
+
+    print("Qual a duração do evento em horas? ")
+    val duracao = scanner.nextLine().toInt()
+
+    val garconsBase = Math.ceil(convidados / 12.0).toInt()
+    val garconsExtras = Math.ceil(duracao / 2.0).toInt()
+    val totalGarcons = garconsBase * garconsExtras
+    val custoGarcons = totalGarcons * 10.5 * duracao
+
+    println("Serão necessários $totalGarcons garçons.")
+    println("Custo: R$ $custoGarcons")
+    println("Agora vamos calcular o buffet do hotel para o evento.")
+
+    // ===== Parte 4: Buffet =====
+    val litrosCafe = convidados * 0.2
+    val litrosAgua = convidados * 0.5
+    val qtdSalgados = convidados * 7
+
+    val custoCafe = litrosCafe * 0.80
+    val custoAgua = litrosAgua * 0.40
+    val custoSalgados = (qtdSalgados / 100.0) * 34.0
+    val custoBuffet = custoCafe + custoAgua + custoSalgados
+
+    println("O evento precisará de ${litrosCafe.toInt()} litros de café, " +
+            "${litrosAgua.toInt()} litros de água, $qtdSalgados salgados.")
+    println("Custo total do buffet: R$ $custoBuffet")
+
+    val custoTotal = custoGarcons + custoBuffet
+
+
+    val evento = Evento(
+        id = contadorEventos++,
+        empresa = empresa,
+        convidados = convidados,
+        local = local,
+        dia = dia,
+        hora = hora,
+        duracao = duracao,
+        totalGarcons = totalGarcons,
+        custoGarcons = custoGarcons,
+        custoBuffet = custoBuffet,
+        custoTotal = custoTotal,
+        cadeirasExtras = cadeirasExtras
+    )
+
+    eventos.add(evento)
+
+    println("\n=== RESUMO FINAL ===")
+    println("ID do evento: ${evento.id}")
+    println("Local: ${evento.local}")
+    println("Empresa: ${evento.empresa}")
+    println("Dia: ${evento.dia} às ${evento.hora}h")
+    println("Convidados: ${evento.convidados}")
+    if (evento.cadeirasExtras > 0) println("Cadeiras extras: ${evento.cadeirasExtras}")
+    println("Garçons: ${evento.totalGarcons} (Custo: R$ ${evento.custoGarcons})")
+    println("Buffet: R$ ${evento.custoBuffet}")
+    println("CUSTO TOTAL DO EVENTO: R$ ${evento.custoTotal}")
+    println("Evento cadastrado com sucesso!\n")
+}
+
+
+
+
 
 fun main() {
     val scanner = Scanner(System.`in`)
